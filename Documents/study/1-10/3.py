4c37d0e74cba4ea9337232197ca15968861002c1
skippers-guitar <ikomahayato@gmail.com>
2023-07-07 05:26:54 +0900

    3.pyを追加

---
+++ Documents/study/1-10/3.py
@@ -0,0 +1,35 @@
+def sentence_reverce(sentence1,sentence2):
+    len_sen1 = len(sentence1)
+    len_sen2 = len(sentence2)
+
+    i = 0
+    cnt1 = 0
+    cnt2 = 0
+
+    new_sentence = ''
+
+    while cnt1 < len_sen1 and cnt2 < len_sen2:
+        if i % 2 == 0: 
+            new_sentence = new_sentence + sentence1[cnt1]
+            cnt1 += 1
+        else:
+            new_sentence = new_sentence + sentence2[cnt2]
+            cnt2 += 1
+        i += 1
+    
+    while cnt1 + cnt2 < len_sen1+len_sen2:
+        if cnt1 < len_sen1:
+            new_sentence = new_sentence + sentence1[cnt1]
+            cnt1 += 1
+        elif cnt2 < len_sen2:
+            new_sentence = new_sentence + sentence2[cnt2]
+            cnt2 += 1
+
+    return new_sentence
+
+if __name__ == "__main__":
+    #文字列
+    sentence1 = 'パトカーあああああああああああ'
+    sentence2 = 'タクシーーーーーー'
+    answer = sentence_reverce(sentence1,sentence2)
+    print(answer)
