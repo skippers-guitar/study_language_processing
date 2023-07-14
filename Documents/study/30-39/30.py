817982ead76e733b0d933b0e092f7d2703faf2bf
skippers-guitar <ikomahayato@gmail.com>
2023-07-15 02:30:49 +0900

    created neko's json-file and 30.py

--- Documents/study/30-39/30.py
+++ Documents/study/30-39/30.py
@@ -1,34 +1,70 @@
 from os import path
 import prepare as prep
 import re
+import json
 
 def text_kakou(text):
-    text = re.sub('(EOS|[\t\f])','',text)
+    text = re.sub('(EOS|[\f])','',text)
     text = re.sub('(\n+)','\n',text)
     text = text.strip()
     return(text)
 
-
-if __name__ == "__main__":
-    file_name = 'neko_10' 
-    text = prep.open_mecab(file_name)
-    
-    text = text_kakou(text)
+def keitaiso_yomikomi(text):
     kaigyou_split = text.split("\n")
 
-    head_chr = [0]
+    end_chr = []
     for index,i in enumerate(kaigyou_split):
         if i.split(',')[1] == '空白' or i.split(',')[1] == '句点':
             if i.split(',')[1] != '空白':            
-                head_chr.append(index + 1)
-    del head_chr[-1]
-
-    all_sentence_list = [len(head_chr)]
-    sentence_cnt = 0
+                end_chr.append(index)
     sentence_list = []
+    onesentence_list = []
     chr_library = {}
-    for index,i in enumerate(kaigyou_split):
-        re.findall('(/+?)/t(.*?),(.*?),(?:.*?),(?:.*?),(?:.*?),(?:.*?),(.*?).+')
-    # 表層形\t品詞大分類,品詞細分類,*,*,活用型,活用形,見出し語,読み,意味情報
+    for index,text in enumerate(kaigyou_split):
+        result_bunkai = re.findall('(.*?)\t(.*?),(.*?),(?:.*?),(?:.*?),(?:.*?),(?:.*?),(.*?),.+', text)
+    # ★表層形\t★品詞大分類,★品詞細分類,*,*,活用型,活用形,★見出し語,読み,意味情報
     # 表層形（surface），基本形（base），品詞（pos），品詞細分類1
+    # 1。\t2記号,3句点,4*,5*,6*,7*,8。,9。,10。
+    # 1(/+?)\t2(.*?),3(.*?),4(?:.*?),5(?:.*?),6(?:.*?),7(?:.*?),8(.*?).+
+        for i in result_bunkai:
+            chr_library["surface"] = i[0] 
+            chr_library["base"] = i[3] 
+            chr_library["pos"] = i[1] 
+            chr_library["pos1"] = i[2] 
+            onesentence_list.append(chr_library)
+            chr_library = {}
+        if index in end_chr:
+            sentence_list.append(onesentence_list)
+            onesentence_list = []
+    return(sentence_list)
+
+def list_json_henkan(list):
+    json = {}
+    cnt = 0
+    for i in list:
+        lib = {}
+        cnt2 = 0
+        for j in i:
+            lib[cnt2] = j
+            cnt2 += 1
+        json[str(cnt) + "行目"] = lib
+        cnt += 1
+    return(json)
+
+if __name__ == "__main__":
+    file_name = 'neko' 
+    text = prep.open_mecab(file_name)
+    
+    text = text_kakou(text)
+    result = keitaiso_yomikomi(text)
+    
+    result_json = list_json_henkan(result)
+
+    b = path.join(path.dirname(__file__), "output30.json")
+    with open(b,'w',encoding='utf-8') as f2:    
+        json.dump(result_json,f2,indent=4,ensure_ascii = False)
+
+
+
+
     
--- Documents/study/30-39/prepare.py
+++ Documents/study/30-39/prepare.py
@@ -1,4 +1,5 @@
 import MeCab
+import re
 import unicodedata
 from os import path
 
@@ -24,6 +25,11 @@ def file_output(aaa,name_type):
     with open(b,'w',encoding='utf-8') as f2:
         f2.write(aaa) 
 
+def file_output_tuiki(aaa,name_type):
+    b = path.join(path.dirname(__file__), name_type)
+    with open(b,'a',encoding='utf-8') as f2:
+        f2.write(aaa) 
+
 def keitaiso_analysis(file):
     text = open_txt(file)
     mecab_mode = 'mecabrc'
@@ -31,6 +37,54 @@ def keitaiso_analysis(file):
     node = keitaiso.parse(text)
     file_output(str(node),file + ".mecab")
     return(node)
+
+def text_kakou(text):
+    text = re.sub('(EOS|[\f])','',text)
+    text = re.sub('(\n+)','\n',text)
+    text = text.strip()
+    return(text)
+
+def keitaiso_yomikomi(text):
+    kaigyou_split = text.split("\n")
+
+    end_chr = []
+    for index,i in enumerate(kaigyou_split):
+        if i.split(',')[1] == '空白' or i.split(',')[1] == '句点':
+            if i.split(',')[1] != '空白':            
+                end_chr.append(index)
+    sentence_list = []
+    onesentence_list = []
+    chr_library = {}
+    for index,text in enumerate(kaigyou_split):
+        result_bunkai = re.findall('(.*?)\t(.*?),(.*?),(?:.*?),(?:.*?),(?:.*?),(?:.*?),(.*?),.+', text)
+    # ★表層形\t★品詞大分類,★品詞細分類,*,*,活用型,活用形,★見出し語,読み,意味情報
+    # 表層形（surface），基本形（base），品詞（pos），品詞細分類1
+    # 1。\t2記号,3句点,4*,5*,6*,7*,8。,9。,10。
+    # 1(/+?)\t2(.*?),3(.*?),4(?:.*?),5(?:.*?),6(?:.*?),7(?:.*?),8(.*?).+
+        for i in result_bunkai:
+            chr_library["surface"] = i[0] 
+            chr_library["base"] = i[3] 
+            chr_library["pos"] = i[1] 
+            chr_library["pos1"] = i[2] 
+            onesentence_list.append(chr_library)
+            chr_library = {}
+        if index in end_chr:
+            sentence_list.append(onesentence_list)
+            onesentence_list = []
+    return(sentence_list)
+
+def list_json_henkan(list):
+    json = {}
+    cnt = 0
+    for i in list:
+        lib = {}
+        cnt2 = 0
+        for j in i:
+            lib[cnt2] = j
+            cnt2 += 1
+        json[str(cnt) + "行目"] = lib
+        cnt += 1
+    return(json)
 
 if __name__ == "__main__":
     file = "neko_1.txt"
